1c5086dfedd117e67553b3a6afb2c623a11ff946
Euan Brown <3015729b@student.gla.ac.uk>
2026-02-12 21:22:44 +0000

    Chapter 10 complete all tests passed

--- rango/views.py
+++ rango/views.py
@@ -12,17 +12,20 @@ from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    
-    return render(request, 'rango/index.html', context=context_dict)
+
+    visitor_cookie_handler(request)
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
 
 def show_category(request, category_name_slug):
 
@@ -137,6 +140,26 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
@@ -146,5 +169,11 @@ def user_logout(request):
     return redirect(reverse('rango:index'))
 
 def about(request):
-    context_dict = {'boldmessage' : 'Euan'}
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+        
+    visitor_cookie_handler(request)
+    visits = request.session.get('visits', 1)
+    context_dict = {'boldmessage' : 'Euan', 'visits': visits}
     return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -124,6 +124,8 @@ MEDIA_URL = '/media/'
 
 LOGIN_URL = 'rango:login'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
