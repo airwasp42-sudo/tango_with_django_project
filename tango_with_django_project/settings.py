25c2d1cf522252f18d94dcc05466a3f05f90a44f
Euan Brown <3015729b@student.gla.ac.uk>
2026-02-12 20:42:49 +0000

    Chapter 9 complete all tests passed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
